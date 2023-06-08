479a2df0d898c7466d236596c0cb899e22c8530f
Sheron4ik <sheronov.d@mail.ru>
2023-06-08 12:47:29 +0300

    add 3.1

---
+++ module 3/part1.py
@@ -0,0 +1,13 @@
+def predicate_div():
+    def func(num1, num2):
+        return num1 % num2 == 0
+
+    num1, num2 = int(input()), int(input())
+    if func(num1, num2):
+        print("делится")
+    else:
+        print("не делится")
+
+
+if __name__ == '__main__':
+    predicate_div()
